004ae2140a09fa5477f841b785b7090d62231a03
T6776 <ec2-user@ip-172-31-24-5.ec2.internal>
2021-11-14 10:08:56 +0000

    [Update]

---
+++ EC/app/controllers/cart_items_controller.rb
@@ -0,0 +1,29 @@
+class CartItemsController < ApplicationController
+
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.save
+    redirect_to cart_items_path
+  end
+
+  def index
+    @cart_items = CartItem.all
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
+  
+end
+
+
+private
+def cart_item_params
+  params.require(:cart_item).permit(:item_id, :amount)
+end
---
+++ EC/app/controllers/items_controller.rb
@@ -0,0 +1,40 @@
+class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
+end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price)
+  end
